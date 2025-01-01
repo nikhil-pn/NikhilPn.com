c17aed779af8844a133e0b1244d480ae4f0773a3
Nikhil PN <nikhilpn725@gmail.com>
2025-01-01 17:51:40 +0530

    bio updated

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
-import Link from 'next/link'
-import Particles from './components/particles'
+import Link from "next/link";
+import Particles from "./components/particles";
 
 const navigation = [
   { name: "Projects", href: "/projects" },
@@ -8,18 +8,20 @@ const navigation = [
 ];
 
 export default function Home() {
-
-
   return (
     <>
-    {/* centering classes justify-center flex flex-col  md:flex-row md:block */}
+      {/* centering classes justify-center flex flex-col  md:flex-row md:block */}
       <div className="bg-black h-screen ">
         <div className="relative isolate px-6 pt-20 lg:px-8">
-          <div className="absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80" aria-hidden="true">
+          <div
+            className="absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80"
+            aria-hidden="true"
+          >
             <div
               className="relative left-[calc(50%-11rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#7cc9fc] to-[#99a8ff] opacity-30 sm:left-[calc(50%-30rem)] sm:w-[72.1875rem]"
               style={{
-                clipPath: "polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)"
+                clipPath:
+                  "polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)",
               }}
             />
           </div>
@@ -50,43 +52,24 @@ export default function Home() {
 
             <div className="md:my-16 my-24   text-center animate-fade-in">
               <h2 className="text-sm text-zinc-500 ">
-                Hi, my name is Nikhil Pn, And im a twitter addict {""}
-                <Link
-                  target="_blank"
-                  href="https://www.h3lios.in/"
-                  className="underline duration-500 hover:text-zinc-300"
-                >
-                  H3lios.in
-                </Link>
-
-                <br />
-                and Contributing to openSource{" "}
-                <Link
-                  target="_blank"
-                  href="https://athenafoss.com"
-                  className="underline duration-500 hover:text-zinc-300"
-                >
-                  unkey.dev
-                </Link>{" "}
-                at night.
-
-
-
+                Hi, my name is Nikhil Pn, and I'm a Twitter addict
               </h2>
             </div>
           </div>
-          <div className="absolute inset-x-0 top-[calc(100%-13rem)] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[calc(80%-30rem)]" aria-hidden="true">
+          <div
+            className="absolute inset-x-0 top-[calc(100%-13rem)] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[calc(80%-30rem)]"
+            aria-hidden="true"
+          >
             <div
               className="relative left-[calc(50%+3rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 bg-gradient-to-tr from-[#fc6dc8] to-[#fc6d76] opacity-30 sm:left-[calc(50%+36rem)] sm:w-[72.1875rem]"
               style={{
-                clipPath: "polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)"
+                clipPath:
+                  "polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)",
               }}
             />
           </div>
         </div>
       </div>
-
-
     </>
-  )
+  );
 }
